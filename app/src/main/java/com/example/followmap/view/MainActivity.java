d2167a0762a43e32a839b022027f5d8564b5c27e
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-16 16:33:25 -0400

    Uso do ToggleEnabled em tilSenha e Criação de OnClick para botões da MainActivity

--- app/src/main/java/com/example/followmap/view/MainActivity.java
+++ app/src/main/java/com/example/followmap/view/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.followmap.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.followmap.R;
 import com.example.followmap.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +14,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        binding.btnEntrar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Intent it = new Intent(MainActivity.this, ...);
+//            }
+//        });
+
+        binding.tvCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, RegistroDeUsuarioActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
