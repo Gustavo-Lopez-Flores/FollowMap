6280987f4fdf2f959ecf81d6dac1ecf4895da558
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-16 17:30:03 -0400

    Criação de UsuárioDao

---
+++ app/src/main/java/com/example/followmap/dao/UsuarioDao.java
@@ -0,0 +1,33 @@
+package com.example.followmap.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.followmap.entities.Usuario;
+
+import java.util.List;
+
+@Dao
+public interface UsuarioDao {
+
+    @Insert
+    void insert(Usuario usuario);
+
+    @Update
+    void update(Usuario usuario);
+
+    @Delete
+    void delete(Usuario usuario);
+
+    @Query("SELECT * FROM Usuario WHERE email = :email AND senha = :senha")
+    Usuario login(String email, String senha);
+
+    @Query("SELECT * FROM Usuario")
+    List<Usuario> getAllUsuarios();
+
+    @Query("SELECT * FROM Usuario WHERE usuarioId = :id")
+    Usuario getUsuarioById(int id);
+}
