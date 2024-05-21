45f8d68ff73114ff89e497a40084037caf3265c4
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-21 17:42:22 -0400

    flag de database em LocalDatabase

--- app/src/main/java/com/example/followmap/database/LocalDatabase.java
+++ app/src/main/java/com/example/followmap/database/LocalDatabase.java
@@ -2,11 +2,14 @@ package com.example.followmap.database;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.followmap.dao.UsuarioDao;
+import com.example.followmap.entities.Usuario;
 
+@Database(entities = {Usuario.class}, version = 1)
 public abstract class LocalDatabase extends RoomDatabase{
     public abstract UsuarioDao usuarioDao();
     private static LocalDatabase INSTANCE;
