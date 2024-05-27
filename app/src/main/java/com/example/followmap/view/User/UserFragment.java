9edba06617171fc8d5d1705875a6d59b604218d5
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-26 21:14:34 -0400

    metodo preencherUsuarios criando ArrayAdapter e direcionar para UserView se algum item for selecionado

--- app/src/main/java/com/example/followmap/view/User/UserFragment.java
+++ app/src/main/java/com/example/followmap/view/User/UserFragment.java
@@ -5,15 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.followmap.R;
 import com.example.followmap.databinding.FragmentUserBinding;
+import com.example.followmap.entities.Usuario;
+
+import java.util.List;
 
 public class UserFragment extends Fragment {
 
@@ -41,8 +44,32 @@ public class UserFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        preencheUsuarios();
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void preencheUsuarios() {
+        userViewModel.getUsuarios().observe(getViewLifecycleOwner(), new Observer<List<Usuario>>() {
+            @Override
+            public void onChanged(List<Usuario> usuarios) {
+                ArrayAdapter<Usuario> usuariosAdapter = new ArrayAdapter<>(getContext(),
+                        android.R.layout.simple_list_item_1, usuarios);
+                listViewUsuarios.setAdapter(usuariosAdapter);
+
+                listViewUsuarios.setOnItemClickListener((parent, view, position, id) -> {
+                    Usuario usuarioSelecionado = usuarios.get(position);
+                    Intent intent = new Intent(getActivity(), UserView.class);
+                    intent.putExtra("USUARIO_SELECIONADO_ID", usuarioSelecionado.getUsuarioId());
+                    startActivity(intent);
+                });
+            }
+        });
     }
 }
