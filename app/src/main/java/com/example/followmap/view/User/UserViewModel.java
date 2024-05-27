d70ae6a1b18f0bef849919d11ea65c26ed99e802
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-26 20:34:21 -0400

    substituição de extends ViewModel por AndroidViewModel, assim haverá um ApplicationContext permitindo acesso ao BD

--- app/src/main/java/com/example/followmap/view/User/UserViewModel.java
+++ app/src/main/java/com/example/followmap/view/User/UserViewModel.java
@@ -1,21 +1,24 @@
 package com.example.followmap.view.User;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.example.followmap.database.LocalDatabase;
 import com.example.followmap.entities.Usuario;
 
 import java.util.List;
 
-public class UserViewModel extends ViewModel {
+public class UserViewModel extends AndroidViewModel {
 
     private final MutableLiveData<List<Usuario>> usuarios;
     private final LocalDatabase db;
 
-    public UserViewModel() {
-        db = LocalDatabase.getDatabase(getApplicationContext());
+    public UserViewModel(Application application) {
+        super(application);
+        db = LocalDatabase.getDatabase(application);
         usuarios = new MutableLiveData<>();
         carregarUsuarios();
     }
