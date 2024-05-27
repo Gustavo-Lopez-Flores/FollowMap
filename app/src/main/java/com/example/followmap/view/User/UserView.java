6e12a6905a68a11fd76e1f4e3b5d7c24935ebd90
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-26 22:10:49 -0400

    app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/followmap/view/User/UserView.java
+++ app/src/main/java/com/example/followmap/view/User/UserView.java
@@ -2,6 +2,7 @@ package com.example.followmap.view.User;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -39,5 +40,35 @@ public class UserView extends AppCompatActivity {
         binding.edtNomeUsuario.setText(dbUsuario.getNome());
         binding.edtEmailUsuario.setText(dbUsuario.getEmail());
         binding.edtSenhaUsuario.setText(dbUsuario.getSenha());
+    }
+
+    public void salvarUsuario(View view) {
+        String nome = binding.edtNomeUsuario.getText().toString();
+        String email = binding.edtNomeUsuario.getText().toString();
+        String senha = binding.edtNomeUsuario.getText().toString();
+
+        if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+            Toast.makeText(this, "Preencha todos os campos.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.edtEmailUsuario.setError("Email inválido.");
+            return;
+        } else {
+            binding.edtEmailUsuario.setError(null);
+        }
+
+        Usuario thisUsuario = new Usuario(nome, email, senha);
+
+        if (dbUsuario != null) {
+            thisUsuario.setUsuarioId(dbUsuarioID);
+            db.usuarioDao().update(thisUsuario);
+            Toast.makeText(this, "Dados do usuário atualizados com sucesso.", Toast.LENGTH_SHORT).show();
+        } else {
+            db.usuarioDao().insert(thisUsuario);
+            Toast.makeText(this, "Usuário criado com sucesso.", Toast.LENGTH_SHORT).show();
+        }
+        finish();
     }
 }
