bffdc1c3c83a57b3e09af1c0f207ba34bae12e03
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-26 21:09:48 -0400

    onCreate de fragmentUsuario para carregar usuários a partir de método da ViewModel

--- app/src/main/java/com/example/followmap/view/User/UserFragment.java
+++ app/src/main/java/com/example/followmap/view/User/UserFragment.java
@@ -1,31 +1,42 @@
 package com.example.followmap.view.User;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.followmap.R;
 import com.example.followmap.databinding.FragmentUserBinding;
 
 public class UserFragment extends Fragment {
 
     private FragmentUserBinding binding;
+    private ListView listViewUsuarios;
+    private UserViewModel userViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        UserViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(UserViewModel.class);
-
         binding = FragmentUserBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textUser;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        listViewUsuarios = binding.listViewUsuarios;
+
+        binding.btnAddUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UserView.class);
+                startActivity(intent);
+            }
+        });
+
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         return root;
     }
 
