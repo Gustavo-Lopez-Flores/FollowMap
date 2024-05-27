cfc57adb6dc897c2f71e02d98a675b789f8631e5
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-27 01:09:47 -0400

    Migração de BD Room para lidar com mudanças no esquema e tratar a IllegalStateException

--- app/src/main/java/com/example/followmap/database/LocalDatabase.java
+++ app/src/main/java/com/example/followmap/database/LocalDatabase.java
@@ -2,9 +2,12 @@ package com.example.followmap.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.followmap.dao.UsuarioDao;
 import com.example.followmap.dao.EnderecoDao;
@@ -13,17 +16,32 @@ import com.example.followmap.entities.Usuario;
 import com.example.followmap.entities.Endereco;
 import com.example.followmap.entities.Cidade;
 
-@Database(entities = {Usuario.class, Cidade.class, Endereco.class}, version = 1)
-public abstract class LocalDatabase extends RoomDatabase{
+@Database(entities = {Usuario.class, Cidade.class, Endereco.class}, version = 2)
+public abstract class LocalDatabase extends RoomDatabase {
     public abstract UsuarioDao usuarioDao();
     public abstract CidadeDao cidadeDao();
     public abstract EnderecoDao enderecoDao();
     private static LocalDatabase INSTANCE;
 
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Cidade` (`cidadeId` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `cidade` TEXT, `estado` TEXT)");
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Endereco` (`enderecoId` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `descricao` TEXT, `latitude` REAL NOT NULL, `longitude` REAL NOT NULL, `cidadeId` INTEGER NOT NULL, FOREIGN KEY(`cidadeId`) REFERENCES `Cidade`(`cidadeId`) ON UPDATE NO ACTION ON DELETE CASCADE)");
+        }
+    };
+
     public static LocalDatabase getDatabase(Context context) {
-        if(INSTANCE == null){
-            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    LocalDatabase.class, "MeuBD").allowMainThreadQueries().build();
+        if (INSTANCE == null) {
+            synchronized (LocalDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    LocalDatabase.class, "MeuBD")
+                            .addMigrations(MIGRATION_1_2)
+                            .allowMainThreadQueries()
+                            .build();
+                }
+            }
         }
         return INSTANCE;
     }
