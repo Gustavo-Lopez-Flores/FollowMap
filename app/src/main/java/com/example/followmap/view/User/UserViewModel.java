60adcec8432b9bc447455b95810cc0a32778187e
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-26 20:30:51 -0400

    método carregar usuários em UserViewModel

--- app/src/main/java/com/example/followmap/view/User/UserViewModel.java
+++ app/src/main/java/com/example/followmap/view/User/UserViewModel.java
@@ -4,16 +4,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.followmap.database.LocalDatabase;
+import com.example.followmap.entities.Usuario;
+
+import java.util.List;
+
 public class UserViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<Usuario>> usuarios;
+    private final LocalDatabase db;
 
     public UserViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        db = LocalDatabase.getDatabase(getApplicationContext());
+        usuarios = new MutableLiveData<>();
+        carregarUsuarios();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    private void carregarUsuarios() {
+        List<Usuario> userList = db.usuarioDao().getAllUsuarios();
+        usuarios.setValue(userList);
+    }
+
+    public LiveData<List<Usuario>> getUsuarios() {
+        return usuarios;
     }
 }
