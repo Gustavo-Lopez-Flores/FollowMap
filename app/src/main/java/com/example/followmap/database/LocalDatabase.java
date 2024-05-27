b198fd6bac2dc928b4e4eddaed00eae26bbe52eb
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-26 23:24:32 -0400

    atualização de DB com inclusão dos DAOs de Endereco e Cidade

--- app/src/main/java/com/example/followmap/database/LocalDatabase.java
+++ app/src/main/java/com/example/followmap/database/LocalDatabase.java
@@ -7,11 +7,17 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.followmap.dao.UsuarioDao;
+import com.example.followmap.dao.EnderecoDao;
+import com.example.followmap.dao.CidadeDao;
 import com.example.followmap.entities.Usuario;
+import com.example.followmap.entities.Endereco;
+import com.example.followmap.entities.Cidade;
 
-@Database(entities = {Usuario.class}, version = 1)
+@Database(entities = {Usuario.class, Cidade.class, Endereco.class}, version = 1)
 public abstract class LocalDatabase extends RoomDatabase{
     public abstract UsuarioDao usuarioDao();
+    public abstract CidadeDao cidadeDao();
+    public abstract EnderecoDao enderecoDao();
     private static LocalDatabase INSTANCE;
 
     public static LocalDatabase getDatabase(Context context) {
